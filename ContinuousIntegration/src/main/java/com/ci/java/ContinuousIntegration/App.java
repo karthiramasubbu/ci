cf2f338b43a5df9a5bbfb87cbadb7fb16c92bada
karthiramasubbu <rkarthee@hotmail.com>
2013-12-24 18:02:49 -0500

    jenkins test

--- ContinuousIntegration/src/main/java/com/ci/java/ContinuousIntegration/App.java
+++ ContinuousIntegration/src/main/java/com/ci/java/ContinuousIntegration/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Welcome Continuous Integration World" );
+        System.out.println("There is some change in the code -- now the jenkins job should build");
     }
 }
